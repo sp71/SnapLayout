55d61431e762cfebc3a6459ea34e9ae24727803b
Satinder Singh <satindersingh71@gmail.com>
2017-10-14 17:22:22 -0700

    Updated Project for Mac OS

--- SnapLayout/Classes/Aliases.swift
+++ SnapLayout/Classes/Aliases.swift
@@ -7,17 +7,19 @@
 //
 
 #if os(iOS) || os(tvOS)
+    import UIKit
     /// Allows views to be OS independent while leveraging existing code
     public typealias View = UIView
     /// Allows layout priority to be OS independent while leveraging existing code
     public typealias LayoutPriority = UILayoutPriority
     /// Allows layout priority required to be OS independent while leveraging existing code
     public var LayoutPriorityRequired = UILayoutPriority.required
-#elseif os(OSX)
+#else
+    import AppKit
     /// Allows views to be OS independent while leveraging existing code
     public typealias View = NSView
     /// Allows layout priority to be OS independent while leveraging existing code
-    public typealias LayoutPriority = NSLayoutPriority
+    public typealias LayoutPriority = NSLayoutConstraint.Priority
     /// Allows layout priority required to be OS independent while leveraging existing code
-    public var LayoutPriorityRequired = NSLayoutPriorityRequired
+    public var LayoutPriorityRequired = NSLayoutConstraint.Priority.required
 #endif
